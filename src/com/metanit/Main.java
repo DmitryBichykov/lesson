e9846af4152332870c3f19ac067700c8e4927c3b
Dmitry <dmitry.bichikov@gmail.com>
2020-03-24 13:02:50 +0300

    lessons

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,31 @@
+package com.metanit;
+
+
+public class Main {
+    private static StringBuilder line = new StringBuilder("Мама мылы раму! Раму мыла мама. Сколько тебе лет? " +
+            "я пойду гулять по гаю. Гай красивый? Очень! Ох, как хорошо. ");
+    private static int number=0;
+
+    public static void main(String[] args) {
+        System.out.println("Исходная строка:\n"+line);
+        search(".");
+        search("!");
+        search("?");
+
+        System.out.println("Количество предложений в тексте: "+number);
+        }
+
+
+    private static void search(String str){
+        int index;
+        int pos=0;
+         do{
+            index=line.substring(pos).indexOf(str);
+            if ( index!= -1) {
+                number++;
+                pos+=index+1;
+            }
+        }while (index!=-1);
+    }
+
+}
